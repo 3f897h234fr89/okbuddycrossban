670301af6b077ad2d6bad61d5a0aedcd9ef4788f
A. A <123700@students.alvinisd.net>
2021-07-17 17:13:37 -0500

    Update fully to d.js v13

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({intents:[]});
+const client = new Discord.Client({intents:['GUILDS', 'GUILD_BANS']});
 
 const config = require('./config.json');
 const NodeCache = require( "node-cache" );
@@ -9,29 +9,34 @@ const myCache = new NodeCache();
 var staffChannelsMap = new Map(Object.entries(JSON.parse(fs.readFileSync('guilds.json', 'utf-8'))));
 
 const createButton = (label, style, ID) => {
-    return new MessageButton()
+    return new Discord.MessageButton()
         .setLabel(label)
         .setStyle(style)
-        .setCustomID(ID);
+        .setCustomId(ID);
 }
 
-client.on('clickButton', async (interaction) => {
-    if (interaction.isButton()) {
-        const { channel, member, guild, id, message } = interaction;
-        const args = id.split('-');
 
-        clicker.fetch().then(() => {
-            const member = guild.members.cache.get(clicker.id);
-            if (!member.hasPermission('BAN_MEMBERS')) {
-                channel.send(`<@${clicker.id}> you need the permission \`BAN_MEMBERS\` to be able to interact with this bot`);
+function registerGuild(interaction) {
+    staffChannelsMap[interaction.guildId] = interaction.channelId;
+}
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isButton()) {
+        const { channel, member, guild, customId, message } = interaction;
+        const args = customId.split('-');
+
+        member.fetch().then(() => {
+            const banned = guild.members.cache.get(member.id);
+            if (!banned.permissions.has(Discord.Permissions.FLAGS.BAN_MEMBERS)) {
+                channel.send(`<@${member.id}> you need the permission \`BAN_MEMBERS\` to be able to interact with this bot`);
                 return;
             }
         });
 
         switch (args[0]) {
             case 'delete_prompt' :
-                clicker.fetch().then(() => {
-                    channel.send(`<@${clicker.id}> cancelled the sharing of the ban of user <@${args[2]}>`);
+                member.fetch().then(() => {
+                    channel.send(`<@${member.id}> cancelled the sharing of the ban of user <@${args[2]}>`);
                     message.delete();
                 });
             break;
@@ -48,41 +53,41 @@ client.on('clickButton', async (interaction) => {
                     .setFooter('Apply this ban to this server?');
 
                 let yesButton = createButton('Yes', 'SUCCESS', `ban_request-${bannedGuild.id}-${bannedUser.id}`);
-                let noButton = createButton('No', 'FAILURE', `cancel_ban_request-${bannedGuild.id}-${bannedUser.id}`)
+                let noButton = createButton('No', 'DANGER', `cancel_ban_request-${bannedGuild.id}-${bannedUser.id}`)
 
-                let row = new MessageActionRow()
+                let row = new Discord.MessageActionRow()
                     .addComponents(yesButton, noButton);
 
-                channel.send(`<@${clicker.id}> shared the ban of <@${args[2]}>`);
+                channel.send(`<@${member.id}> shared the ban of <@${args[2]}>`);
                 message.delete();
             
-                staffChannelsMap.values().forEach(channel => {
+                for (let channel of staffChannelsMap.values()) {
                     client.channels.fetch(channel).then((channel) => {
-                        channel.guild.fetchBans().then((bans) => { 
+                        channel.guild.bans.fetch().then((bans) => { 
                             if(bans.get(bannedUser.id)) {
                                 return;
                             } else {
-                            channel.send(sharedEmbed, row);
+                                channel.send({ embeds: [shareEmbed], components: [row]});
                         }
                         });
                     });
-                });
+                }
             break;
 
             case 'cancel_ban_request': 
-                clicker.fetch().then(() => {
-                    channel.send(`<@${clicker.id}> rejected the shared ban of <@${args[2]}>`);
+                member.fetch().then(() => {
+                    channel.send(`<@${member.id}> rejected the shared ban of <@${args[2]}>`);
                     message.delete();
                 });
             break;
 
             case 'ban_request' :
-                clicker.fetch().then(() => {
+                member.fetch().then(() => {
                     const userToBan = client.users.cache.get(args[2]);
                     const originGuild = client.guilds.cache.get(args[1]);
                     const reason = `Cross ban from ${originGuild.name}: ` + myCache.get(args[2]);
         
-                    channel.send(`<@${clicker.id}> has accepted the shared ban reqeust of <@${args[2]}> from ${originGuild.name}`);
+                    channel.send(`<@${member.id}> has accepted the shared ban reqeust of <@${args[2]}> from ${originGuild.name}`);
                     message.delete();
                     
                     guild.members.ban(userToBan.id, {reason: reason}).then(user => {
@@ -105,10 +110,12 @@ client.on('clickButton', async (interaction) => {
     }
 });
 
-client.on('guildBanAdd', (guild, user) => {
-    guild.fetchBans().then((bans) => {
-        const reason = bans.get(user.id).reason;
+client.on('guildBanAdd', (ban) => {
+    const guild = ban.guild
+    const user = ban.user
 
+    guild.bans.fetch(user.id).then((banInfo) => {
+        const reason = banInfo.reason;
         if (reason.includes('Cross ban')) {
             return;
         }
@@ -120,14 +127,14 @@ client.on('guildBanAdd', (guild, user) => {
             .setFooter('Share this ban with the other servers?');
 
         let shareButton = createButton('Share', 'SUCCESS', `share_prompt-${guild.id}-${user.id}`);
-        let cancelButton = createButton('Cancel', 'FAILURE', `delete_prompt-${guild.id}-${user.id}`);
-        let row = new MessageActionRow()
+        let cancelButton = createButton('Cancel', 'DANGER', `delete_prompt-${guild.id}-${user.id}`);
+        let row = new Discord.MessageActionRow()
             .addComponents(shareButton, cancelButton);
         
         myCache.set(user.id, reason);
 
         client.channels.fetch(staffChannelsMap.get(guild.id)).then((channel) => {
-            channel.send(shareEmbed, row);
+            channel.send({ embeds: [shareEmbed], components: [row]});
         });
     });
 });
@@ -144,11 +151,6 @@ client.once('ready', () => {
     // TODO: move this to global and not use specific guild
     client.guilds.cache.get('854851946855006238').commands.create(commandData);
 });
-
-function registerGuild(interaction) {
-    staffChannelsMap[interaction.guild.id] = interaction.channel.id;
-}
-
 client.login(config.token);
 
 // save the guild map on exit
