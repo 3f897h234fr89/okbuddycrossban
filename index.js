b72574ec32246f58a69621a5d4c0804254f40fd3
3f897h234fr89 <84993606+3f897h234fr89@users.noreply.github.com>
2021-07-23 21:28:25 +0200

    major code refactor

--- index.js
+++ index.js
@@ -1,25 +1,85 @@
-const { Client, Intents } = require('discord.js');
-global.Discord = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const intents = new Intents();
-intents.add('GUILDS', 'GUILD_BANS', 'GUILD_MESSAGES');
+intents.add('GUILDS', 'GUILD_BANS');
 const client = new Client({ intents: intents });
 
-const fs = require('fs');
 require('dotenv').config();
-const NodeCache = require( "node-cache" );
+const NodeCache = require('node-cache');
 global.cache = new NodeCache();
- 
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-events.forEach(eventFile => {
-    const event = require(`./events/${eventFile}`);
-    client.on(event.name, (...args) => event.callback(...args, client));
+
+const Redis = require('ioredis');
+const redis = new Redis(process.env.REDIS_PATH);
+
+const adler32 = require('adler32');
+const debugGuildId = '865190393221873714';
+
+const app = (guildID) => {
+    return guildID ? client.api.applications(client.user.id).guilds(guildID) : client.api.applications(client.user.id);
+}
+
+client.once('ready', async () => {
+    console.log(`${client.user.tag} is now online.`);
+    client.user.setActivity('THE WORLD BURN', {
+        type: 'WATCHING'
+    });
 });
 
-client.commands = new Discord.Collection();
-const commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-commands.forEach(file => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+client.on('guildCreate', (guild) => {
+    // TODO: Add command creation logic
+    app(guild.id).commands.post({
+        data: {
+            name: 'ping',
+            description: 'Gets the current bot response time in milliseconds.'
+        }
+    });
+
+    app(guild.id).commands.post({
+        data: {
+            name: 'register',
+            description: 'Request to register the server with the network.',
+            options: [
+                {
+                    name: 'channel',
+                    description: 'Your server\'s staff channel',
+                    required: true,
+                    type: 7
+                },
+                {
+                    name: 'message',
+                    description: 'Optional register request message.',
+                    required: false,
+                    type: 3
+                }
+            ]
+        }
+    })
 });
 
-client.login(process.env.token);
+client.on('guildBanAdd', (ban) => {
+    // TODO: Leave the guild if server is not registered in
+    // the network.
+    const [guild, user] = [ban.guild, ban.user];
+
+    guild.bans.fetch(user.id).then(banInfo => {
+        const reason = banInfo.reason;
+        const key = adler32.sum(user.id + guild.id);
+        const banData = {
+            user: user,
+            guild: guild,
+            reason: reason
+        }
+        cache.set(key, banData);
+    });
+
+    // TODO: Send embeds
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if(interaction.isButton()) {
+        // TODO: Implement button logic
+    } else {
+        // TODO: Implement command logic
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
