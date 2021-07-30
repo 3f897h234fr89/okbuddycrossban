fe2194b7ea753b53f2c142ef96a619c70902dcd9
paisu46 <88111626+paisu46@users.noreply.github.com>
2021-07-30 22:05:18 +0200

    remove legacy code

--- registerCommands.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-    client.api.applications(client.user.id).commands.post({
-        data: {
-            name: 'ping',
-            description: 'Gets the current bot response time in ms'
-        }
-    })
-}
